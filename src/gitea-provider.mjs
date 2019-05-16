ab9d018b1bfad916d22e10f709e699b68c128415
Markus Felten <Markus.Felten@gmx.de>
2019-05-16 02:36:17 +0200

    fix: add api env

--- src/gitea-provider.mjs
+++ src/gitea-provider.mjs
@@ -1,4 +1,5 @@
-import { Provider, Repository, Branch } from 'repository-provider';
+import { Provider, Repository, Branch } from "repository-provider";
+import fetch from "node-fetch";
 
 /**
  * Gitea provider
@@ -6,13 +7,23 @@ import { Provider, Repository, Branch } from 'repository-provider';
  */
 export class GiteaProvider extends Provider {
   /**
-   * provide token from one of
+   * provide token and api from one of
    * - GITEA_TOKEN
+   * - GITEA_API
    * @param {Object} env process env
    * @return {Object} with auth token
    */
   static optionsFromEnvironment(env) {
     const token = env.GITEA_TOKEN;
-    return token === undefined ? undefined : { auth: token };
+    const api = env.GITEA_API;
+    return token === undefined ? undefined : { auth: token, api };
+  }
+
+  async *repositories(pattern) {
+    const result = await fetch(this.api + "repos/search");
+    repos = await result.json(result);
+    for (const r of result) {
+      yield r;
+    }
   }
 }
