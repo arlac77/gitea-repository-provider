cb09e4bb8c6977fcb9fa5c67c2067bca30181082
Markus Felten <Markus.Felten@gmx.de>
2019-05-19 22:47:24 +0200

    wip

--- src/gitea-provider.mjs
+++ src/gitea-provider.mjs
@@ -16,11 +16,23 @@ export class GiteaProvider extends Provider {
   static optionsFromEnvironment(env) {
     const token = env.GITEA_TOKEN;
     const api = env.GITEA_API;
-    return token === undefined ? undefined : { auth: token, api };
+    return { token, api };
+  }
+
+  static get defaultOptions() {
+    return {
+      api: undefined,
+      token: undefined,
+      ...super.defaultOptions
+    };
   }
 
   async *repositories(pattern) {
-    const result = await fetch(this.api + "repos/search");
+    const result = await fetch(this.api + "repos/search", {
+      headers: { authorization: "token " + this.token },
+      accept: "application/json"
+    });
+    console.log(result);
     repos = await result.json(result);
     for (const r of result) {
       yield r;
--- tests/gitea-provider-test.mjs
+++ tests/gitea-provider-test.mjs
@@ -1,10 +1,14 @@
-import test from 'ava';
-import { GiteaProvider } from '../src/gitea-provider';
+import test from "ava";
+import { GiteaProvider } from "../src/gitea-provider";
 
-const config = GiteaProvider.optionsFromEnvironment({ GITEA_TOKEN: '123456' });
+const config = GiteaProvider.optionsFromEnvironment({
+  GITEA_TOKEN: "123456",
+  GITEA_API: "http://mydomain.com/gitea/v1/"
+});
 
-test('provider constructor', async t => {
+test("provider constructor", async t => {
   const provider = new GiteaProvider(config);
 
-  t.is(provider.config.token, '123456');
+  t.is(provider.token, "123456");
+  t.is(provider.api, "http://mydomain.com/gitea/v1/");
 });
