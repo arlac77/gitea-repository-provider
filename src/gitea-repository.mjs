fc3990397d9faf1fc3547f2e0f19b7057b22549e
Markus Felten <Markus.Felten@gmx.de>
2019-09-19 19:33:16 +0200

    fix: check for array response

--- src/gitea-repository.mjs
+++ src/gitea-repository.mjs
@@ -49,8 +49,6 @@ export class GiteaRepository extends Repository {
 
     const data = await result.json();
 
-    //console.log(data);
-
     if(Array.isArray(data)) {
       return data[0].object.sha;
     }
