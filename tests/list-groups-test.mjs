190ed0fc86d9b54527cb3ee85709eed454a0cd1a
Markus Felten <Markus.Felten@gmx.de>
2019-10-08 21:57:37 +0200

    test: make use of listGroupsTest

--- tests/list-groups-test.mjs
+++ tests/list-groups-test.mjs
@@ -1,17 +1,12 @@
 import test from "ava";
+import { listGroupsTest } from "repository-provider-test-support";
 import { GiteaProvider } from "../src/gitea-provider.mjs";
 
-test("list groups", async t => {
-  const provider = GiteaProvider.initialize(undefined, process.env);
 
-  let rgs = {};
-
-  for await (const g of provider.repositoryGroups()) {
-    rgs[g.name] = g;
-  }
-
-  t.true(Object.keys(rgs).length > 0);
-  t.is(rgs["markus"].name, "markus");
-  t.is(rgs["markus"].username, "markus");
-  t.is(rgs["github-mirror"].description, "github.com mirror");
-});
+test(
+  listGroupsTest,
+  GiteaProvider.initialize(undefined, process.env),
+  undefined,
+  { markus: { name: "markus", username: "markus" },
+  "github-mirror": { description: "github.com mirror" }  }
+);
