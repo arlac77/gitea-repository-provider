48d40006c448c7bf563a0572d92e8c826c5d460d
Markus Felten <Markus.Felten@gmx.de>
2019-10-22 01:02:56 +0200

    test: non existing repositories

--- tests/list-repositories-test.mjs
+++ tests/list-repositories-test.mjs
@@ -1,5 +1,50 @@
 import test from "ava";
+import { assertRepo } from "repository-provider-test-support";
 import { GiteaProvider } from "../src/gitea-provider.mjs";
+
+const repoFixtures = {
+  "": undefined,
+  " ": undefined,
+  "https://mfelten.dynv6.net/services/git/markus/de.mfelten.archlinux.git": {
+    branch: "master",
+    fullName: "markus/de.mfelten.archlinux",
+    description: "infrasctucture build on arch linux (arm)",
+    provider: GiteaProvider
+  },
+  "https://mfelten.dynv6.net/services/git/markus/de.mfelten.archlinux": {
+    branch: "master",
+    fullName: "markus/de.mfelten.archlinux",
+    description: "infrasctucture build on arch linux (arm)",
+    provider: GiteaProvider
+  },
+  "https://mfelten.dynv6.net/services/git/markus/de.mfelten.archlinux#master": {
+    branch: "master",
+    fullName: "markus/de.mfelten.archlinux",
+    provider: GiteaProvider
+  },
+  "markus/de.mfelten.archlinux#master": {
+    branch: "master",
+    fullName: "markus/de.mfelten.archlinux",
+    description: "infrasctucture build on arch linux (arm)",
+    provider: GiteaProvider
+  },
+
+  "arlac77/aggregation-repository-provider": {
+ /*   branch: "master",
+    fullName: "arlac77/aggregation-repository-provider"*/
+  }
+};
+
+test("locate repository several", async t => {
+  const provider = GiteaProvider.initialize(undefined, process.env);
+
+  t.plan(13);
+
+  for (const rn of Object.keys(repoFixtures)) {
+    const repository = await provider.repository(rn);
+    await assertRepo(t, repository, repoFixtures[rn], rn);
+  }
+});
 
 test("list repositories", async t => {
   const provider = GiteaProvider.initialize(undefined, process.env);
@@ -13,14 +58,14 @@ test("list repositories", async t => {
   }
 
   t.true(Object.keys(rps).length > 0);
-  t.is(rps['markus/Omnia'].fullName,'markus/Omnia');
+  t.is(rps["markus/Omnia"].fullName, "markus/Omnia");
 
-   rps = {};
+  rps = {};
 
-  for await (const r of provider.repositories(['markus/*'])) {
+  for await (const r of provider.repositories(["markus/*"])) {
     rps[r.fullName] = r;
   }
 
   t.true(Object.keys(rps).length > 0);
-  t.is(rps['markus/Omnia'].fullName,'markus/Omnia');
+  t.is(rps["markus/Omnia"].fullName, "markus/Omnia");
 });
