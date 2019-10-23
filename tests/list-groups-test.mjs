d0890f0b9befa243ced32e54ede9c94314638f33
Markus Felten <Markus.Felten@gmx.de>
2019-10-23 23:20:48 +0200

    test: several lists

--- tests/list-groups-test.mjs
+++ tests/list-groups-test.mjs
@@ -2,11 +2,16 @@ import test from "ava";
 import { listGroupsTest } from "repository-provider-test-support";
 import { GiteaProvider } from "../src/gitea-provider.mjs";
 
+const provider = GiteaProvider.initialize(undefined, process.env);
 
-test(
-  listGroupsTest,
-  GiteaProvider.initialize(undefined, process.env),
-  undefined,
-  { markus: { name: "markus", username: "markus" },
-  "github-mirror": { description: "github.com mirror" }  }
-);
+const fullResult = {
+  markus: { name: "markus", username: "markus" },
+  "github-mirror": { description: "github.com mirror" }
+};
+
+test(listGroupsTest, provider, undefined, fullResult);
+test.serial("2nd. time", listGroupsTest, provider, undefined, fullResult);
+test.serial(listGroupsTest, provider, "*", fullResult);
+test.serial(listGroupsTest, provider, "markus", {
+  markus: { name: "markus", username: "markus" }
+});
