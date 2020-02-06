e148663edfe8de9377775aa1a2c1fee516eae3c9
Markus Felten <Markus.Felten@gmx.de>
2020-02-06 02:16:57 +0100

    test: common branch-entry-list-test

---
+++ tests/branch-entry-list-test.mjs
@@ -0,0 +1,30 @@
+import test from "ava";
+import { GiteaProvider } from "../src/gitea-provider.mjs";
+import { entryListTest } from "repository-provider-test-support";
+
+const REPOSITORY_NAME = "https://mfelten.dynv6.net/services/git/markus/sync-test-repository";
+
+const config = GiteaProvider.optionsFromEnvironment(process.env);
+
+test("branch entries list", async t => {
+  const provider = new GiteaProvider(config);
+  const repository = await provider.repository(REPOSITORY_NAME);
+  const branch = await repository.branch("master");
+  await entryListTest(t, branch, undefined, {
+    "README.md": { startsWith: "fil" },
+    "tests/rollup.config.js": { startsWith: "import babel" },
+    tests: { isCollection: true },
+    "a/b/c/file.txt": { startsWith: "file content" }
+  });
+});
+
+test("branch entries list with pattern", async t => {
+  const provider = new GiteaProvider(config);
+  const repository = await provider.repository(REPOSITORY_NAME);
+  const branch = await repository.branch("master");
+
+  await entryListTest(t, branch, ["**/*.mjs", "!tests/*.mjs"], {
+    "tests/repository-test.mjs": { notPresent: true },
+    "src/repository.mjs": { startsWith: "import" }
+  });
+});
