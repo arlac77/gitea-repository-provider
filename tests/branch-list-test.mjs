830f4b768b444aef1ae4daa85e23c54b98ad8c20
Markus Felten <Markus.Felten@gmx.de>
2020-06-18 15:26:56 +0200

    test: add branchListTest

---
+++ tests/branch-list-test.mjs
@@ -0,0 +1,9 @@
+import test from "ava";
+import { branchListTest } from "repository-provider-test-support";
+
+import GiteaProvider from "gitea-repository-provider";
+
+const provider = GiteaProvider.initialize({}, process.env);
+
+test(branchListTest, provider, "bad-name/unknown-*", 0, true);
+test(branchListTest, provider, "arlac77/*repository-provider", 0, true);
