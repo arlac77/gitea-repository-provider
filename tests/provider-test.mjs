5fe6496df211a5dc8bb889f89c8be78e8cc2645e
Markus Felten <Markus.Felten@gmx.de>
2020-11-03 18:57:46 +0100

    test: repositoryBases

--- tests/provider-test.mjs
+++ tests/provider-test.mjs
@@ -6,30 +6,31 @@ test(providerTest, new GiteaProvider());
 
 const config = GiteaProvider.optionsFromEnvironment({
   GITEA_TOKEN: "123456",
-  GITEA_API: "http://mydomain.com/gitea/v1/"
+  GITEA_API: "http://mydomain.com/gitea/api/v1/"
 });
 
 test("provider env options", t => {
   t.deepEqual(config, {
     token: "123456",
-    api: "http://mydomain.com/gitea/v1/"
+    api: "http://mydomain.com/gitea/api/v1/"
   });
 });
 
 test("provider constructor", t => {
   const provider = new GiteaProvider(config);
-
   t.is(provider.token, "123456");
-  t.is(provider.api, "http://mydomain.com/gitea/v1/");
+  t.is(provider.api, "http://mydomain.com/gitea/api/v1/");
+  t.deepEqual(provider.repositoryBases, ["http://mydomain.com/gitea/"]);
 });
 
 test("initialize", t => {
   let provider = GiteaProvider.initialize(undefined, {
     GITEA_TOKEN: "123456",
-    GITEA_API: "http://mydomain.com/gitea/v1"
+    GITEA_API: "http://mydomain.com/gitea/api/v1"
   });
   t.is(provider.name, "gitea");
-  t.is(provider.api, "http://mydomain.com/gitea/v1/");
+  t.is(provider.api, "http://mydomain.com/gitea/api/v1/");
+  t.deepEqual(provider.repositoryBases, ["http://mydomain.com/gitea/"]);
   t.is(provider.token, "123456");
 
   provider = GiteaProvider.initialize(undefined, {
