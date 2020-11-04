9f125ca1f7f52b7e1f18ea40780d56e76c8f9d62
Markus Felten <Markus.Felten@gmx.de>
2020-11-04 14:16:21 +0100

    fix: User is_admin

--- src/gitea-user.mjs
+++ src/gitea-user.mjs
@@ -17,7 +17,8 @@ export class GiteaUser extends RepositoryGroup {
   static get attributeMapping() {
     return {
       ...super.attributeMapping,
-      avatar_url: "avatarURL"
+      avatar_url: "avatarURL",
+      is_admin: "isAdmin"
     };
   }
 }
