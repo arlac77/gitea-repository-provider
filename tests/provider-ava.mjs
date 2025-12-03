e32b2d9ceabcce17acf0a4752e70080b0a9b86c3
Markus Felten <Markus.Felten@gmx.de>
2025-12-03 10:24:22 +0000

    test: with https

--- tests/provider-ava.mjs
+++ tests/provider-ava.mjs
@@ -13,16 +13,16 @@ test(providerTest, new GiteaProvider());
 
 const config = {
   token: "123456",
-  api: "http://mydomain.com/gitea/api/v1/"
+  api: "https://mydomain.com/gitea/api/v1/"
 };
 
 test("provider constructor", t => {
   const provider = new GiteaProvider(config);
   t.is(provider.token, "123456");
-  t.is(provider.api, "http://mydomain.com/gitea/api/v1/");
+  t.is(provider.api, "https://mydomain.com/gitea/api/v1/");
   t.deepEqual(provider.repositoryBases, [
     "gitea:",
-    "http://mydomain.com/gitea/"
+    "https://mydomain.com/gitea/"
   ]);
 });
 
@@ -31,14 +31,14 @@ test("initialize", t => {
     { messageDestination },
     {
       GITEA_TOKEN: "123456",
-      GITEA_API: "http://mydomain.com/gitea/api/v1"
+      GITEA_API: "https://mydomain.com/gitea/api/v1"
     }
   );
   t.is(provider.name, "gitea");
-  t.is(provider.api, "http://mydomain.com/gitea/api/v1/");
+  t.is(provider.api, "https://mydomain.com/gitea/api/v1/");
   t.deepEqual(provider.repositoryBases, [
     "gitea:",
-    "http://mydomain.com/gitea/"
+    "https://mydomain.com/gitea/"
   ]);
   t.is(provider.token, "123456");
 
