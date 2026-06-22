e60822c9b8064ada50ec7ca66ce9aaf6afbd1cc2
Markus Felten <Markus.Felten@gmx.de>
2026-06-22 13:43:29 +0100

    fix: typo

--- src/gitea-organization.mjs
+++ src/gitea-organization.mjs
@@ -22,8 +22,11 @@ export class GiteaOrganization extends RepositoryGroup {
     },
     username: username_attribute,
     email: email_attribute,
-    location: {...string_attribute, name: "location" },
-    visibility: {...string_attribute, name: "visibility" },
-    repo_admin_change_team_access: boolean_attribute, name: "repo_admin_change_team_access" }
+    location: { ...string_attribute, name: "location" },
+    visibility: { ...string_attribute, name: "visibility" },
+    repo_admin_change_team_access: {
+      ...boolean_attribute,
+      name: "repo_admin_change_team_access"
+    }
   };
 }
